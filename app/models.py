c2ee4ef9fddf75b4150bcf1a4e901079d10767cf
ramos peredo <andrewmcpray@gmail.com>
2020-05-01 20:51:05 -0300

    forein
    bug forein

--- app/models.py
+++ app/models.py
@@ -18,37 +18,37 @@ class Tarjeta(models.Model):
     numero = models.IntegerField(max_length=16)
     clave = models.IntegerField(max_length=3)
     fechaVencimiento = models.IntegerField(max_length=4)
-    idTipoTarjeta = models.ForeignKey(TipoTarjeta)
+    idTipoTarjeta = models.ForeignKey(TipoTarjeta,on_delete=models.CASCADE)
 
 class Suscriptor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     clave = models.CharField(max_length=256)
     email = models.CharField(max_length=100)
-    idTarjeta = models.ForeignKey(Tarjeta)
+    idTarjeta = models.ForeignKey(Tarjeta,on_delete=models.CASCADE)
     premium = models.IntegerField(max_length=1)
 
 class Perfil(models.Model):
     nombre = models.CharField(max_length=50)
     foto = models.CharField(max_length=50)
-    idSuscriptor = models.ForeignKey(Suscriptor)
+    idSuscriptor = models.ForeignKey(Suscriptor,on_delete=models.CASCADE)
 
 class Configuracion(models.Model):
     maximoPremium= models.IntegerField(max_length=1)
     maximoStandar= models.IntegerField(max_length=1)
 
 class Favoritos(models.Model):
-    idPerfil = models.ForeignKey(Perfil)
-    idLibro = models.ForeignKey(Libro)
+    idPerfil = models.ForeignKey(Perfil,on_delete=models.CASCADE)
+    idLibro = models.ForeignKey(Libro,on_delete=models.CASCADE)
 
 class Recomendaciones(models.Model):
-    idPerfil = models.ForeignKey(Perfil)
-    idLibro = models.ForeignKey(Libro)
+    idPerfil = models.ForeignKey(Perfil,on_delete=models.CASCADE)
+    idLibro = models.ForeignKey(Libro,on_delete=models.CASCADE)
 
 class Capitulos(models.Model):
-    idLibro = models.ForeignKey(Libro)
+    idLibro = models.ForeignKey(Libro,on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100) 
 
 class Historial(models.Model):
-    idPerfil = models.ForeignKey(Perfil)
-    idLibro = models.ForeignKey(Libro)
+    idPerfil = models.ForeignKey(Perfil,on_delete=models.CASCADE)
+    idLibro = models.ForeignKey(Libro,on_delete=models.CASCADE)
