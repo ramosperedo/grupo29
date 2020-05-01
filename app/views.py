0543f0a3268bd242bf125a3cbe16eb86e89a25b1
ramos peredo <andrewmcpray@gmail.com>
2020-05-01 15:03:33 -0300

    borron y cuenta nueva
    añado un login y su logout y un crear usuario, diferencia de usuarios normales y administradores

---
+++ app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth import logout as do_logout
+
+
+def welcome(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser == 1:
+            return render(request, "users/welcome.html")
+        else:
+            return render(request, "users/home.html")
+    return redirect('/login')
+
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                do_login(request, user)
+                return redirect('/')
+
+    form.fields['username'].help_text = None
+    form.fields['password1'].help_text = None
+    form.fields['password2'].help_text = None
+
+    return render(request, "users/register.html", {'form': form})
+
+
+def login(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                do_login(request, user)
+                return redirect('/')
+    return render(request, "users/login.html", {'form': form})
+
+
+def logout(request):
+    do_logout(request)
+    return redirect('/')
--- grupo29/App/Controllers/IndexController.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render,HttpResponse
-class IndexController():
-    def index(request):
-        #return HttpResponse('<h5> ppp </h5>')
-        return render(request,'views/index/index.html')
