aa6fda4b4e846fe339d5c856ab817a289c45dace
luis neyra <luisneyralp@gmail.com>
2020-05-04 20:37:49 -0300

    resetNow

--- app/models.py
+++ app/models.py
@@ -1,16 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager
+)
 
 # Create your models here.
 
-class Libro(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    autor = models.CharField(max_length=50)
-    isbn = models.IntegerField(unique=True)
-    genero = models.CharField(max_length=20)
-    editorial = models.CharField(max_length=20)
-    descripcion = models.CharField(max_length=250)
-    foto = models.CharField(max_length=100, null=True)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False, is_superuser=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+
+        user_obj = self.model(
+            email = self.normalize_email(email)
+        )
+        user_obj.set_password(password) # change user password
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.superuser = is_superuser
+        user_obj.save(using=self._db)
+        return user_obj
+    
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+                email,
+                password=password,
+                is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+                email,
+                password=password,
+                is_staff=True,
+                is_admin=True,
+                is_superuser=True
+        )
+        return user
 
 class TipoTarjeta(models.Model):
     id = models.AutoField(primary_key=True)
@@ -23,6 +52,68 @@ class Tarjeta(models.Model):
     clave = models.IntegerField()
     fechaVencimiento = models.IntegerField()
     idTipoTarjeta = models.ForeignKey(TipoTarjeta,on_delete=models.CASCADE)
+
+class User(AbstractBaseUser):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255,unique=True)
+    active = models.BooleanField(default=True) #can login
+    staff = models.BooleanField(default=False) #staff user non superuser
+    admin = models.BooleanField(default=False) #superuser
+    superuser = models.BooleanField(default=False)
+    idTarjeta = models.ForeignKey(Tarjeta,on_delete=models.CASCADE, null=True)
+    premium = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email' #username
+    #USERNAME_FIELD and password are required by default
+    REQUIRED_FIELDS = [] #'full_name' python manage.py createsuperuser
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.nombre + self.apellido
+
+    def get_short_name(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.admin
+
+    def has_module_perms(self, app_Label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+    
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_superuser(self):
+        return self.superuser    
+    
+    @property
+    def is_premium(self):
+        return self.premium
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    isbn = models.IntegerField(unique=True)
+    genero = models.CharField(max_length=20)
+    editorial = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=250)
+    foto = models.CharField(max_length=100, null=True)
 
 class Suscriptor(models.Model):
     id = models.AutoField(primary_key=True)
